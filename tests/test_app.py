effcb8d17b550d5a9f4b8701347dd41d15520100
dcerv005 <dacervantes12@gmail.com>
2024-08-21 00:58:41 -0700

    fix bug

--- tests/test_app.py
+++ tests/test_app.py
@@ -12,7 +12,7 @@ class TestAPI(unittest.TestCase):
         num2 = fake.random_number(digits=3)
         print(num1, num2)
         payload = {'num1': num1, 'num2': num2}
-        response = self.app.post('/sum', payload)
+        response = self.app.post('/sum', json=payload)
         data = response.get_json()
         self.assertEqual(data['result'], num1 + num2)
 
